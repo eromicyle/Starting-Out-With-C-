2a6d1a75abb237eb9e85fe2cf16f22661336e754
Eromicyle Xenon <143354285+eromicyle@users.noreply.github.com>
2025-02-25 23:54:56 +0500

    Update 04 Resturant Bill.cpp

--- 04 Resturant Bill.cpp
+++ 04 Resturant Bill.cpp
@@ -1,5 +1,5 @@
 /*Restaurant Bill
-Write a program that computes the tax and tip on a restaurant bill for a patron with a
+Q. Write a program that computes the tax and tip on a restaurant bill for a patron with a
 $44.50 meal charge. The tax should be 6.75 percent of the meal cost. The tip should
 be 15 percent of the total after adding the tax. Display the meal cost, tax amount, tip
 amount, and total bill on the screen*/
