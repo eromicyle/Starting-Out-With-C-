7ee29d060803b4e538373afc4ef4f6057cf4e4ea
Eromicyle Xenon <143354285+eromicyle@users.noreply.github.com>
2025-02-25 23:54:35 +0500

    Update 02 Sales Prediction.cpp

--- 02 Sales Prediction.cpp
+++ 02 Sales Prediction.cpp
@@ -1,5 +1,5 @@
 /*Sales Prediction
-The East Coast sales division of a company generates 62 percent of total sales. Based on that percentage, 
+Q. The East Coast sales division of a company generates 62 percent of total sales. Based on that percentage, 
 write a program that will predict how much the East Coast division will generate if the company has $4.6 million in sales this year.*/
 
 #include <iostream>
